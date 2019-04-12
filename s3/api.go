d9ff6be75a6b58d1ddf6296b71843e23964ad871
Francesco Fucile <francesco.fucile.96@gmail.com>
2019-04-12 16:59:52 +0200

    'put' & 'get' funcs added to api.

--- s3/api.go
+++ s3/api.go
@@ -1,21 +1,74 @@
 package s3
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"io/ioutil"
+	"net/http"
 )
 
-// S3 represents a configurable interface to communicate with S3.
-type S3 struct {
-	AwsConfig aws.Config
+// Bucket represents a configurable interface to an S3 bucket.
+type Bucket struct {
+	Name string
 }
 
-// Default generates an S3 interface using default values
-func (s *S3) Default() {
-	s.AwsConfig = aws.Config{
+// LocalSession returns a session configured to work with LocalStack.
+func LocalSession() *session.Session {
+	return session.Must(session.NewSession(&aws.Config{
 		Credentials:      credentials.NewStaticCredentials(defId, defEndpoint, defToken),
 		S3ForcePathStyle: aws.Bool(true),
 		Region:           aws.String(defRegion),
 		Endpoint:         aws.String(defEndpoint),
+	}))
+}
+
+// Get executes a download from S3.
+func (s *Bucket) Get(key string, data []byte) (out []byte, err error) {
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(s.Name),
+		Key:    aws.String(key),
 	}
+	svc := s3.New(LocalSession())
+	result, err := svc.GetObject(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchKey:
+				return nil, fmt.Errorf(s3.ErrCodeNoSuchKey, aerr.Error())
+			default:
+				return nil, fmt.Errorf(aerr.Error())
+			}
+		} else {
+			return nil, fmt.Errorf(err.Error())
+		}
+		return
+	}
+	body, err := ioutil.ReadAll(result.Body)
+	if err != nil {
+		return nil, fmt.Errorf(err.Error())
+	}
+	return body, nil
+}
+
+// Put uploads a file to S3.
+func (s *Bucket) Put(key string, data []byte) (err error) {
+	_, err = s3.New(LocalSession()).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(s.Name),
+		Key:                  aws.String(key),
+		ACL:                  aws.String("private"),
+		Body:                 bytes.NewReader(data),
+		ContentLength:        aws.Int64(int64(len(data))),
+		ContentType:          aws.String(http.DetectContentType(data)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+	})
+	if err != nil {
+		return fmt.Errorf("cannot upload to S3: " + err.Error())
+	}
+	return nil
 }
